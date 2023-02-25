9b73d7ad4588f05cebc18830fe4c555f152c3e63
Olena Voroshnina <yuriyilena67@gmail.com>
2023-02-25 23:30:35 +0100

    up

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { store } from './redux/store';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter
-    basename="/goit-react-hw-06-phonebook"
+    basename="/goit-react-hw-07-phonebook"
     >
       <Provider store={store}>
         <App />
